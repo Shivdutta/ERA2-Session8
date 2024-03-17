0a12c47a4dc690f80444c3594c248693eafc0a3b
Shivdutta <shivdutta@gmail.com>
2024-03-18 02:36:32 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -4,13 +4,123 @@ import torch.nn.functional as F
 from tqdm import tqdm
 from torchsummary import summary
 
+"""************************************************Session8 Start************************************************"""
+
+#Squeeze and Expand -- Xmas tree
 class Session8_Model(nn.Module):    
-    def __init__(self,dropout_value):
+    def __init__(self, normalization='batch'):
+        # Initialize the Module class
         super(Session8_Model, self).__init__()
-        pass
-    
+
+        # Dropout value of 10%
+        self.dropout_value = 0.1
+        self.number_of_groups = 2
+
+        # Convolutional Block-1
+        self.conv_block1 = self.custom_conv_block(in_channels=3, out_channels=20, kernel_size=3, padding=1, normalization=normalization)
+        self.conv_block2 = self.custom_conv_block(in_channels=20, out_channels=20, kernel_size=3, padding=1, normalization=normalization)
+
+        # Transitional Block-1
+        self.conv_point1 = self.custom_conv_block(in_channels=20, out_channels=16, kernel_size=1, padding=0, normalization=normalization)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Convolutional Block-2
+        self.conv_block3 = self.custom_conv_block(in_channels=16, out_channels=26, kernel_size=3, padding=1, normalization=normalization)
+        self.conv_block4 = self.custom_conv_block(in_channels=26, out_channels=26, kernel_size=3, padding=1, normalization=normalization)
+        self.conv_block5 = self.custom_conv_block(in_channels=26, out_channels=26, kernel_size=3, padding=1, normalization=normalization)
+
+        # Transitional Block-2
+        self.conv_point2 = self.custom_conv_block(in_channels=26, out_channels=16, kernel_size=1, padding=0, normalization=normalization)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Convolutional Block-3
+        self.conv_block7 = self.custom_conv_block(in_channels=16, out_channels=32, kernel_size=3, padding=1, normalization=normalization)
+        self.conv_block8 = self.custom_conv_block(in_channels=32, out_channels=32, kernel_size=3, padding=1, normalization=normalization)
+        self.conv_block9 = self.custom_conv_block(in_channels=32, out_channels=32, kernel_size=3, padding=1, normalization=normalization)
+
+        # Global Average Pooling
+        self.gap = nn.Sequential(nn.AdaptiveAvgPool2d(1))
+
+        # Output Layer
+        self.conv_block10 = self.custom_conv_block(in_channels=32, out_channels=10, kernel_size=1, padding=0, last_layer=True, normalization=normalization)
+
     def forward(self, x):
-        pass
+        # Convolutional Block-1
+        x = self.conv_block1(x)
+        x = x + self.conv_block2(x)
+
+        # Transitional Block-
+        x = self.conv_point1(x)
+        x = self.pool1(x)
+
+        # Convolutional Block-2
+        x = self.conv_block3(x)
+        x = x + self.conv_block4(x)
+        x = x + self.conv_block5(x)
+
+        # Transitional Block-2
+        x = self.conv_point2(x)
+        x = self.pool2(x)
+
+        # Convolutional Block-3
+        x = self.conv_block7(x)
+        x = x + self.conv_block8(x)
+        x = x + self.conv_block9(x)
+
+        x = self.gap(x)
+        x = self.conv_block10(x)
+
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=1)
+
+
+    def get_normalization_layer(self,normalization,out_channels,number_of_groups = None):
+        # Select normalization type
+        if normalization == "layer":
+            _norm_layer = nn.GroupNorm(1, out_channels)
+        elif normalization == "group":
+            if not number_of_groups:
+                raise ValueError("Value of group is not defined")
+            _norm_layer = nn.GroupNorm(number_of_groups, out_channels)
+        else:
+            _norm_layer = nn.BatchNorm2d(out_channels)
+        
+        return _norm_layer
+
+    def custom_conv_block(self, in_channels: int,
+                            out_channels: int,
+                            kernel_size: int = 3,
+                            padding: int = 0,
+                            stride: int = 1,
+                            normalization: str = "batch",
+                            last_layer: bool = False):
+        """
+        Method to return a standard convolution block
+        :param in_channels: Number of input channels
+        :param out_channels: Number of output channels
+        :param kernel_size: Size of the kernel used in the layer
+        :param padding: Padding used in the layer
+        :param stride: Stride used for convolution
+        :param normalization: Type of normalization technique used
+        :param last_layer: Flag to indicate if the layer is last convolutional layer of the network
+        """
+        _norm_layer = self.get_normalization_layer(self,normalization,out_channels,self.number_of_groups)
+
+        # For last layer only return the convolution output
+        if last_layer:
+            return nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, stride=stride, kernel_size=kernel_size, bias=False, padding=padding))
+        
+        return nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, stride=stride, kernel_size=kernel_size, bias=False, padding=padding),
+            nn.ReLU(),
+            _norm_layer,
+            nn.Dropout(self.dropout_value)
+        )
+
+
+
+"""************************************************Session8 End************************************************"""
+
 
 """************************************************Session7 Start************************************************"""
 class Session7_Model10(nn.Module):
